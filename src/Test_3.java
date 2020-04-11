47834fbdbda5d52c96103a99539ff3e83def873d
Mars-Miku <29676159+Mars-Miku@users.noreply.github.com>
2020-04-11 17:12:51 +0800

    2020/4/11

---
+++ src/Test_3.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution_3 {
+    public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
+
+public class Test_3 {
+    public static String stringToString(String input) {
+        if (input == null) {
+            return "null";
+        }
+        return input;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+
+            int ret = new Solution_3().romanToInt(s);
+
+            String out = String.valueOf(ret);
+
+            System.out.println(out);
+        }
+    }
+}
