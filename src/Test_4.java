a64a44e2f34f70e2a831c7557590944c6011d2fc
Mars-Miku <29676159+Mars-Miku@users.noreply.github.com>
2020-04-12 20:04:22 +0800

    2020/4/12

---
+++ src/Test_4.java
@@ -0,0 +1,26 @@
+//LeetCode最长公共前缀
+
+import java.io.IOException;
+
+class Solution {
+    public String longestCommonPrefix(String[] str) {
+        if (str.length == 0) return "";
+        String prefix = str[0];
+        for (int i = 1; i < str.length; i++)
+            while (str[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
+
+public class Test_4 {
+    public static void main(String[] args) {
+        String[] str = {"flower", "flow", "flight"};
+
+        String out = (new Solution().longestCommonPrefix(str));
+
+        System.out.print(out);
+    }
+}
